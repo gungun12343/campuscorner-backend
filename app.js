c84ae9e7c2ca24a81c0bb32963800bb3a8118ae9
Gungun Koshta <koshtagungun795@gmail.com>
2025-08-04 15:59:59 +0530

    made change

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ async function main() {
   await mongoose.connect(db_URL);
 }
 
-app.use(cors({ origin: 'https://campuscorner-frontend.vercel.app/', credentials: true}));
+app.use(cors({ origin: 'https://campuscorner-frontend.vercel.app', credentials: true}));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
